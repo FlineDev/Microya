397e5d02f8fb845ee380fb6822d03e62126d6f85
Cihat Gündüz <commits@cihatguenduez.de>
2021-09-30 13:23:53 +0200

    Remove no longer necessary LinuxMain file
    Newer versions of Swift (package manager) use automated test discovery on Linux by default.

--- build-script.sh
+++ build-script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-sourcery --sources Tests/MicroyaTests --templates .sourcery/LinuxMain.stencil --output Tests/LinuxMain.swift
 swift-format --recursive Sources/Microya Tests --in-place
 swift-format lint --recursive Sources/Microya Tests
