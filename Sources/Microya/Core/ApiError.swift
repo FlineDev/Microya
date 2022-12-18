7c16302453d57ca80cd560c19725781b2d5a3a05
Cihat Gündüz <commits@cihatguenduez.de>
2022-12-18 23:01:53 +0900

    Update dependencies + add error description for each possible case

--- Sources/Microya/Core/ApiError.swift
+++ Sources/Microya/Core/ApiError.swift
@@ -54,3 +54,40 @@ extension ApiError: Equatable where ClientErrorType: Equatable {
     }
   }
 }
+
+extension ApiError {
+   public var errorDescription: String {
+      switch self {
+      case .noResponseReceived(let error):
+         if let error {
+            return
+               "The request was sent, but the server response was not received. Please check your internet connection. System error: \(error.localizedDescription)"
+         } else {
+            return "The request was sent, but the server response was not received. Please check your internet connection."
+         }
+
+      case .noDataInResponse(let statusCode):
+         return
+            "The request was sent and the server responded with status code \(statusCode), but the response did not include any body although a body was requested."
+
+      case .responseDataConversionFailed(let type, let error):
+         return
+            "The request was sent and the server responded with a body, but the conversion of the body to the expected type \(type.description) failed. System error: \(error.localizedDescription)"
+
+      case .clientError(let statusCode, _):
+         return "The request was sent but the server responded with status code \(statusCode), which means that something is wrong with the request."
+
+      case .serverError(let statusCode):
+         return "The request was sent but the server responded with status code \(statusCode), which means an internal issue occurred on the server."
+
+      case .unexpectedStatusCode(let statusCode):
+         return "The request was sent but the server responded with the unexpected status code \(statusCode)."
+
+      case .unexpectedResponseType(let response):
+         return "Server responded with a non HTTP response, although an HTTP request was made. Either a bug in `JsonApi` or on the server side. Response: \(response)"
+
+      case .emptyMockedResponse:
+         return "The `mockingBehavior` was set to non-nil (for testing) but no `mockedResponse` was provided for the requested endpoint."
+      }
+   }
+}
